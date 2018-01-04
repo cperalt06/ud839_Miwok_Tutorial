7221c6ac5b5357f546f6da866e1a63b523ad0f93
Carlos <cperalt06@gmail.com>
2018-01-04 14:21:36 -0500

    Initial Commit with Completeness Up to Lesson 2

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,48 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by carlos on 12/28/17.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word>{
+    public WordAdapter(@NonNull Context context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        return super.getView(position, convertView, parent);
+        // Check if the view is currently being reused, otherwise inflate the view.
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false
+            );
+        }
+
+        // Get the {@link WordAdapter} object located at this position in the list.
+        Word currentWord = getItem(position);
+
+        // Find the textView in the list_item.xml layout with the ID name
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        return listItemView;
+
+    }
+}
