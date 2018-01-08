558aa95a4df30a60a343ebfd05f4f5a6c7b5b3b6
Carlos <cperalt06@gmail.com>
2018-01-08 15:46:43 -0500

    Add comment for the second constructor

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -42,6 +42,14 @@ public class Word {
         mMiwokTranslation = miwokTranslation;
     }
 
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     * @param imageResourceId is the drawable resource ID for image associated with it
+     */
     public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
