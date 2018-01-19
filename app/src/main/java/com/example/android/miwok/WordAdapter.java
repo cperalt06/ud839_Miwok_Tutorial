4999f87e7f15ede4846eab54b82390649be8af08
Carlos <cperalt06@citymail.cuny.edu>
2018-01-19 14:42:14 -0500

    Modified the list items to display the category color's

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +13,12 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-/**
- * Created by carlos on 12/28/17.
- */
-
 public class WordAdapter extends ArrayAdapter<Word>{
-    public WordAdapter(@NonNull Context context, ArrayList<Word> words) {
+    private int mBackgroundColor;
+
+    public WordAdapter(@NonNull Context context, ArrayList<Word> words, int backgroundColor) {
         super(context, 0, words);
+        mBackgroundColor = backgroundColor;
     }
 
     @NonNull
@@ -32,6 +32,7 @@ public class WordAdapter extends ArrayAdapter<Word>{
                     R.layout.list_item, parent, false
             );
         }
+
 
         // Get the {@link WordAdapter} object located at this position in the list.
         Word currentWord = getItem(position);
@@ -54,6 +55,10 @@ public class WordAdapter extends ArrayAdapter<Word>{
         else {
             iconView.setVisibility(View.GONE);
         }
+
+        View list_background = listItemView.findViewById(R.id.list_background);
+        int color = ContextCompat.getColor(getContext(), mBackgroundColor);
+        list_background.setBackgroundColor(color);
 
         return listItemView;
     }
